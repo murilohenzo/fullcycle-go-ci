e63f2a749e8e4603bd7a89de8994889d776d656d
Murilo Henzo <murilohezo@gmail.com>
2022-01-11 10:31:13 -0300

    Is invalid sum

--- math_test.go
+++ math_test.go
@@ -3,11 +3,20 @@ package main
 import "testing"
 
 func TestSum(t *testing.T)  {
-	sum := Sum(15, 15)
-	expected := 30
-
-	if sum != expected {
-		t.Errorf("Invalid sum result: Result %d. Expected %d", sum, expected)
-	}
-
+	t.Run("Is valid sum", func(t *testing.T) {
+		sum := Sum(15, 15)
+		expected := 30
+	
+		if sum != expected {
+			t.Errorf("Invalid sum result: Result %d. Expected %d", sum, expected)
+		}
+	})
+	t.Run("Is invalid sum", func(t *testing.T) {
+		sum := Sum(14, 15)
+		expected := 30
+	
+		if sum != expected {
+			t.Errorf("Invalid sum result: Result %d. Expected %d", sum, expected)
+		}
+	})
 }
